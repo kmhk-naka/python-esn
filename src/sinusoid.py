2452e146ad09a1ab225e654e2bf29e6213fb0fd6
kmhk-naka <phytran35@gmail.com>
2021-01-18 22:29:12 +0900

    add example; sinusoid

---
+++ src/sinusoid.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from esn import EchoStateNetwork as ESN
+
+
+image_path = Path('./images').resolve()
+image_path.mkdir(mode=0o775, parents=True, exist_ok=True)
+
+init_length = 1000
+train_length = 3000
+test_length = 1000
+tmax = train_length + test_length
+
+x = np.linspace(0, 100 * np.pi, tmax)
+data = np.sin(x).reshape(-1, 1)
+
+model = ESN(
+    init_length=init_length,
+    train_length=train_length,
+    test_length=test_length,
+    tmax=tmax
+)
+
+d = data[init_length + 1:train_length + 1]
+
+model.fit(data[:train_length], d)
+predict_result = model.predict(data[train_length:])
+
+# Plot options
+bbox_to_anchor = (1.02, 0)
+bbox_loc = 'lower left'
+
+fig = plt.figure(figsize=(10, 8))
+
+# Plot prediction
+ax1 = fig.add_subplot(211)
+index = np.arange(train_length, tmax)
+ax1.plot(index, data[train_length: train_length + test_length], label='input data')
+ax1.plot(index, predict_result[:, 0], label='predict result')
+ax1.set_ylim(-1.1, 1.1)
+ax1.legend(bbox_to_anchor=bbox_to_anchor, loc=bbox_loc, borderaxespad=0)
+ax1.set_title('Prediction')
+
+# Plot prediction (enlarged)
+ax2 = fig.add_subplot(212)
+zoom_length = 200
+index = np.arange(train_length, train_length + zoom_length)
+ax2.plot(index, data[train_length: train_length + zoom_length], label='input data')
+ax2.plot(index, predict_result[:zoom_length, 0], label='predict result')
+ax2.legend(bbox_to_anchor=bbox_to_anchor, loc=bbox_loc, borderaxespad=0)
+ax1.set_ylim(-1.1, 1.1)
+ax2.set_title('Prediction (enlarged)')
+
+fig.subplots_adjust(right=0.8, hspace=0.5)
+
+plt.savefig(image_path / 'sinusoid.png', bbox_inches='tight', pad_inches=0.1)
